f25605d25ccf3c055bee76f70380d55c47f1678c
AryaAnish121 <73824605+AryaAnish121@users.noreply.github.com>
2021-01-15 18:44:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', function(){
+    navbar.classList.toggle('toggledNav');
+    hamburger.classList.toggle('toggledHam');
+});
+
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 0,
+    modifier: 0,
+    slideShadows: false,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>';
+    },
+  },
+});
